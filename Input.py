1ae96e1208b320739a589a19fdaa3d0998325c10
Abhigyan_Bose <abhigyandeepbose@gmail.com>
2018-04-02 10:27:03 +0530

    Input Accepted
    Additional Sample images added as well.

---
+++ Input.py
@@ -0,0 +1,5 @@
+import cv2
+from Recognise import recognise
+
+img = cv2.imread("Open 1.jpg")
+recognise(img)
--- Recognise.py
+++ Recognise.py
@@ -3,6 +3,8 @@ import cv2
 
 def recognise(img):
     img_gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-    img_filtered = cv2.GaussianBlur(img_gray,10)
+    img_filtered = cv2.GaussianBlur(img_gray,(15,15),3)
     _,img_thresh = cv2.threshold(img_filtered,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
     cv2.imshow('Otsu_Result',img_thresh)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
